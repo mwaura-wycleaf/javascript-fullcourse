dd5cf40024fbede41fe2858bcbb5fa19aa9111e9
mwaura-wycleaf <wycliff.14396@gmail.com>
2026-05-16 20:10:04 +0300

    Seperated the .js file to external file

---
+++ javascript_syntax/script.js
@@ -0,0 +1,21 @@
+ //case sensitivity
+        let lastName = "Mwaura";
+        let lastname = "Njeri";
+     
+        document.getElementById("demo").innerHTML = lastname;
+        
+        //keywords
+        let age = 5;
+        let firstName = "James";
+    
+        document.getElementById("demo").innerHTML = firstName + " is " + age + " years old";
+
+        let x;//Declaration
+        x = 5;//Assignment
+
+        let b = 5;// variable initialization
+
+        let y = 6;
+        //Compute values
+        let z = x + y;
+        console.log(z);
