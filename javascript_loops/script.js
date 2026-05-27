059f94cd1b04f3295c85bad25fdc47182059595d
mwaura-wycleaf <wycliff.14396@gmail.com>
2026-05-27 17:57:24 +0300

    Iterated over object keys using the for...in loop

--- javascript_loops/script.js
+++ javascript_loops/script.js
@@ -1,53 +1,68 @@
 //For loop
-        const cars = ["BMW", "Volvo", "Saab", "Ford"];
-        let len = cars.length;
-        let text = "";
+const cars = ["BMW", "Volvo", "Saab", "Ford"];
+let len = cars.length;
+let text = "";
 
-        for(let i = 0; i < len; i++){
-            text += cars[i] + "<br>";
-        }
+for(let i = 0; i < len; i++){
+    text += cars[i] + "<br>";
+}
 
-        document.getElementById("demo").innerHTML = text;
+document.getElementById("demo").innerHTML = text;
 
-        //while loop
-        let $text = "";
+//while loop
+let $text = "";
 
-        let $i = 0;
-        while ($i < 10){
-            $text += "The number is " + $i + "<br>";
-            $i++;
-        }
+let $i = 0;
+while ($i < 10){
+    $text += "The number is " + $i + "<br>";
+    $i++;
+}
 
-        document.getElementById("demo-1").innerHTML = $text;
+document.getElementById("demo-1").innerHTML = $text;
 
-        //do...while loop
-        let _text = "";
-        let _i = 0;
+//do...while loop
+let _text = "";
+let _i = 0;
 
-        do {
-            _text += "<br>The number is " + _i;
-            _i++;
-        }
-        while(_i <= 10);
+do {
+    _text += "<br>The number is " + _i;
+    _i++;
+}
+while(_i <= 10);
 
-        document.getElementById("demo-2").innerHTML = _text;
+document.getElementById("demo-2").innerHTML = _text;
 
-        //break statement
-        let texty = "";
+//break statement
+let texty = "";
 
-        for (let index = 0; index < 10; index++){
-            if (index === 3){break;}
-            texty += "<br>The number is " + index;
-        }
+for (let index = 0; index < 10; index++){
+    if (index === 3){break;}
+    texty += "<br>The number is " + index;
+}
 
-        document.getElementById("demo-3").innerHTML = texty;
+document.getElementById("demo-3").innerHTML = texty;
 
-        //continue statement
-        let _texty = "";
+//continue statement
+let _texty = "";
 
-        for (let _index = 0; _index < 10; _index++){
-            if (_index === 3){continue;}
-            _texty += "<br>The number is " + _index;
-        }
+for (let _index = 0; _index < 10; _index++){
+    if (_index === 3){continue;}
+    _texty += "<br>The number is " + _index;
+}
 
-        document.getElementById("demo-4").innerHTML = _texty;
+document.getElementById("demo-4").innerHTML = _texty;
+
+//for...in-----it it mostly typicalyy used for iterating over object keys
+const person = {
+    firstName:"John",
+    secondName:"Maina",
+    age:25
+}
+
+let te$xt = "";
+
+for (let x in person){
+   te$xt += person[x] + " ";
+}
+
+console.log(te$xt)
