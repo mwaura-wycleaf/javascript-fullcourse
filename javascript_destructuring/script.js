22025595f725b7b27ca5810c588bc256a5992555
mwaura-wycleaf <wycliff.14396@gmail.com>
2026-06-01 14:28:02 +0300

    Covered array destructuring

--- javascript_destructuring/script.js
+++ javascript_destructuring/script.js
@@ -21,3 +21,11 @@ let myName = "Wycliff";
 let [a1, a2, a3] = myName;
 
 console.log(a2);
+
+//Array destructuring
+
+let fruits = ["Banana", "Apple", "Orange"];
+
+let [fruit1, fruit2] = fruits;
+
+document.querySelector("#demo2").innerHTML = fruit1;
