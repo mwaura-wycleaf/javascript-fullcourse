52f6d2a4ec8730f56bba6796d432e902b2a1a3bd
mwaura-wycleaf <wycliff.14396@gmail.com>
2026-06-03 15:13:36 +0300

    Accessed the third paragraph using its class name

--- html_DOM/script.js
+++ html_DOM/script.js
@@ -7,3 +7,8 @@ firstParagraph.innerHTML = "This paragraph was accessed via the paragraph's id."
 const secondParagraph = document.getElementsByTagName("p");
 
 secondParagraph[1].innerHTML = "This paragraph was accessed using the tag name"
+
+//Getting elements using the tag name
+const thirdParagraph = document.getElementsByClassName("demo2")[0];
+
+thirdParagraph.innerHTML = "This paragraph was acccessed using the className"
