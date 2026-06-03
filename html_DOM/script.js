b9617837e582ecf960ed2d6d880b56d6d2cef51e
mwaura-wycleaf <wycliff.14396@gmail.com>
2026-06-03 15:30:12 +0300

    Accessed element using query selector and introduced querySelectorAll

--- html_DOM/script.js
+++ html_DOM/script.js
@@ -12,3 +12,10 @@ secondParagraph[1].innerHTML = "This paragraph was accessed using the tag name"
 const thirdParagraph = document.getElementsByClassName("demo2")[0];
 
 thirdParagraph.innerHTML = "This paragraph was acccessed using the className"
+
+//querySelector
+document.querySelector("#demo3").innerHTML = "This element was accessed using querySelector"//for class you use .
+
+//querySelectorAll-it is used like querySelector only that in this case there is more than one element withthe same id or class name 
+
+
