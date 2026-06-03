b2f94ab481db7a569763e43ac8a6c4d542b4bbc1
mwaura-wycleaf <wycliff.14396@gmail.com>
2026-06-03 14:46:02 +0300

    Accessed the second paragraph using the tag name

--- html_DOM/script.js
+++ html_DOM/script.js
@@ -2,3 +2,8 @@
 const firstParagraph = document.getElementById("demo")
 
 firstParagraph.innerHTML = "This paragraph was accessed via the paragraph's id."
+
+//Getting Elements by tag name
+const secondParagraph = document.getElementsByTagName("p");
+
+secondParagraph[1].innerHTML = "This paragraph was accessed using the tag name"
