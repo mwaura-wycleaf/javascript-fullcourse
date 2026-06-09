a48349cd8b0f375117b3702113302e96e2f9c796
mwaura-wycleaf <wycliff.14396@gmail.com>
2026-06-09 15:40:50 +0300

    Did a recap on functions

---
+++ advanced functions/script.js
@@ -0,0 +1,28 @@
+//function declaration
+
+function myFunction (x, y) {
+    return x * y;
+}
+
+//function expression (Named)
+const myFunction = function name(x, y) {
+    return x * y;
+}
+
+//function expression (Anonymous)
+const myFunction =  function (x, y) {
+    return x * y;
+}
+
+//Arrow function 
+const myFunction = (x, y) => x * y;
+
+//function constructor
+const myFunction = new Function ("X", "Y", "return x * y");
+
+//object method
+const obj = {
+    myFunction(x, y) {
+        return x * y;
+    }
+}
