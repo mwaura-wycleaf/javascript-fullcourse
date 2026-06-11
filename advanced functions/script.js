71cc023fc9d586495403d05c9b8f0ce846444d44
mwaura-wycleaf <wycliff.14396@gmail.com>
2026-06-11 13:36:16 +0300

    Commented our the recap for the event handling to work in the browser

--- advanced functions/script.js
+++ advanced functions/script.js
@@ -1,31 +1,31 @@
-//function declaration
+// //function declaration
 
-function myFunction (x, y) {
-    return x * y;
-}
+// function myFunction (x, y) {
+//     return x * y;
+// }
 
-//function expression (Named)
-const myFunction = function name(x, y) {
-    return x * y;
-}
+// //function expression (Named)
+// const myFunction = function name(x, y) {
+//     return x * y;
+// }
 
-//function expression (Anonymous)
-const myFunction =  function (x, y) {
-    return x * y;
-}
+// //function expression (Anonymous)
+// const myFunction =  function (x, y) {
+//     return x * y;
+// }
 
-//Arrow function 
-const myFunction = (x, y) => x * y;
+// //Arrow function 
+// const myFunction = (x, y) => x * y;
 
-//function constructor
-const myFunction = new Function ("X", "Y", "return x * y");
+// //function constructor
+// const myFunction = new Function ("X", "Y", "return x * y");
 
-//object method
-const obj = {
-    myFunction(x, y) {
-        return x * y;
-    }
-}
+// //object method
+// const obj = {
+//     myFunction(x, y) {
+//         return x * y;
+//     }
+// }
 
 //Function callbacks
  //event handling
