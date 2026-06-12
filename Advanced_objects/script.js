317a73a0f770e6c7e13b2e94ae356b43e9c3bda7
mwaura-wycleaf <wycliff.14396@gmail.com>
2026-06-12 17:40:23 +0300

    Looped over my object using the for...of loop

---
+++ Advanced_objects/script.js
@@ -0,0 +1,35 @@
+//this in javascript
+const person = {
+    firstName: "John",
+    secondName: "Doe",
+    id:6376,
+    fullName: function() {
+        return this.firstName + " " + this.secondName;//In this case it refers to the object
+    }
+}
+
+document.getElementById("demo").innerHTML = person.fullName();
+
+//object iterations
+
+//Object.assign...copies properties from one or more source objects to a target object
+
+const person1 = {
+    firstName: "Jaime",
+    secondName:"Laurel"
+}
+
+//Assign source to target
+Object.assign(person, person1)
+
+//returns an array of the key/value pairs in an object
+document.getElementById("demo1").innerHTML = Object.entries(person);
+
+
+let text = "";
+
+for (let [title, name] of Object.entries(person1)){
+    text += title + ": " + name + "<br>"
+}
+
+document.getElementById("demo2").innerHTML = text;
