2e77a77157a99e69ec2f091525299bf3d503296b
mwaura-wycleaf <wycliff.14396@gmail.com>
2026-06-13 19:03:23 +0300

    Created a promise and used it with the then() method

--- Asynchronous programming/script.js
+++ Asynchronous programming/script.js
@@ -18,3 +18,26 @@ function myFunction() {
     let time = new Date();
     document.getElementById("demo1").innerHTML = time.getHours() + ": " + time.getMinutes() + ": " + time.getSeconds();
 }
+
+//Javascript promises...a promise is either fulfilled, pending or rejected
+//creating a promise object
+function myDisplayer1(some) {
+    document.querySelector("#demo2").innerHTML += some + " "
+}
+
+let myPromise = new Promise(function(resolve, reject){
+    ok = true;
+
+    if (ok) {
+        resolve("OK")
+    }else {
+        resolve("Error");
+    }
+});
+
+//Using then to display the result
+myPromise.then(
+    function(value){myDisplayer1(value)},
+    function(value){myDisplayer1(value)},
+)
+
