e5dd09cc5a8f9df0e10d3954dfe9c513a71550ce
mwaura-wycleaf <wycliff.14396@gmail.com>
2026-06-14 18:47:52 +0300

    Introduced JSON data types

--- JSON/script.js
+++ JSON/script.js
@@ -1,3 +1,33 @@
 //json syntax...it is almost identical to javaScript objects
 //{"name":"Wycliff"}...JSON
 {name: "Wycliff"}//object.
+
+//JSON data types
+//String
+//{"name":"Wycliff"}
+
+//Numbers
+//{"age": 30};
+
+//Objects
+/*
+{
+    "employee":{"name": 'John', "age":30}
+}
+*/
+
+//Arrays
+/*{
+    "employee":["John", "Anna", "Klaus"];
+}
+*/
+
+//Boolen
+/*{
+    "sale":true;
+}*/
+
+//Null
+/*{
+    "middleName":null
+}*/
