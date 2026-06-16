d8e57cbb8d1491e490782f9b29c4ae267f459e41
mwaura-wycleaf <wycliff.14396@gmail.com>
2026-06-16 18:05:43 +0300

    Fetched using async and await and printed to the console

--- Javascript API/script.js
+++ Javascript API/script.js
@@ -3,3 +3,12 @@ let file = 'rete.txt'
 fetch(file)
 .then(x => x.text())
 .then(y => document.getElementById("demo").innerHTML = y);
+
+//using async
+async function getText(file) {
+    let myObject = await fetch(file);
+    let myText= await myObject.text();
+    console.log(myText)
+}
+
+getText("rete.txt")
