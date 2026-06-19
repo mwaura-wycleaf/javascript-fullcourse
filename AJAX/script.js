b8254dd08aa5eb63b62b28750b1ffa42e5cd13be
mwaura-wycleaf <wycliff.14396@gmail.com>
2026-06-19 17:17:25 +0300

    Used onreadystatechange which is to be executed when the readyState changes

--- AJAX/script.js
+++ AJAX/script.js
@@ -7,3 +7,17 @@ function loadDoc() {
     xhttp.open("GET", "ajax_info.txt");
     xhttp.send();
 }
+
+//AJAX uses a combination of browser built in XMLHttpRequest object(to request data from a web server)
+//The XMLHttpRequest Object
+//onreadystatechange property...defines a callback function to be executed when the readystate changes
+function $loadDoc() {
+    const $xhttp = new XMLHttpRequest();
+    $xhttp.onreadystatechange = function() {
+        if(this.readyState == 4 && this.status == 200) {
+            document.getElementById("demo1").innerHTML = this.responseText;
+        }
+    }
+    $xhttp.open("GET", "ajax_info.txt");
+    $xhttp.send();
+}
